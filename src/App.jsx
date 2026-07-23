df5635788cafcefccbf7be43a3048a2be23f4331
kutya001 <111129686+kutya001@users.noreply.github.com>
2026-07-23 13:45:21 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ export default function App() {
       ua: navigator.userAgent,
       ref: document.referrer || 'Прямой заход (или Instagram DM/Story)',
       screen: `${window.screen.width}x${window.screen.height}`,
-      resumeText: 'Web-версия',
+      resumeText: 'Твое резюме открыли!',
       viewport: `${window.innerWidth}x${window.innerHeight}`,
       pixelRatio: window.devicePixelRatio || 1,
       cores: navigator.hardwareConcurrency || 0,
